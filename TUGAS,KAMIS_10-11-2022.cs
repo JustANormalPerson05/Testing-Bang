4a9c46794afb9b6db2c646e7236ee3237bfdb8c6
JustANormalPerson05 <hayumaxrpl@gmail.com>
2022-11-12 12:13:33 +0700

    Update TUGAS,KAMIS_10-11-2022.cs

--- TUGAS,KAMIS_10-11-2022.cs
+++ TUGAS,KAMIS_10-11-2022.cs
@@ -3,16 +3,17 @@ class AritmatikaPenjumlahan
 {
  public static void Main (string[] args)
  {
-     int mangga, apel, jambu, hasil = 0;
+     int mangga, apel, jambu, Srikaya, hasil = 0;
      Console.Write("Mangga = ");
      mangga = int.Parse(Console.ReadLine());
      Console.Write("apel = ");
      apel = int.Parse(Console.ReadLine());
      Console.Write("jambu = ");
      jambu = int.Parse(Console.ReadLine());
-     
+     Console.Write("Srikaya = ");
+     Srikaya = int.Parse(Console.ReadLine());
      // operasi penjumlahan dengan operator +
-     hasil = mangga + apel + jambu;
-     Console.WriteLine ($"Hasil mangga + apel + jambu = {hasil}");
+     hasil = mangga + apel + jambu + Srikaya;
+     Console.WriteLine ($"Hasil mangga + apel + jambu + Srikaya = {hasil}");
  }
 }
